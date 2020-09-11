8df98cc9074d1c2b004ffbcc74aa94727e9af816
SH4URYA.Singh838 <69413964+Shaurya186@users.noreply.github.com>
2020-09-11 22:31:06 +0530

    Add files via upload

---
+++ pick.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  render() {
+    return (
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+    );
+  }
+}
+
+ReactDOM.render(<Header />, document.getElementById('root'));
